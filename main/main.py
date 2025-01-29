4025712e639813a9cec0e23bf3f7c106814d99e3
Felix Solomon <felix.solom@gmail.com>
2025-01-29 23:36:54 +0200

    started upload

--- main/main.py
+++ main/main.py
@@ -1,9 +1,10 @@
 from typing import Union, Annotated
 
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, UploadFile, Request, HTTPException
 from fastapi.staticfiles import StaticFiles
 from fastapi.requests import Request
 from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
 
 from pydantic import BaseModel
 import cv2
@@ -20,13 +21,40 @@ async def index(request: Request):
     return templates.TemplateResponse(
         "index.html", context={'request':request}
     )
-"""
+
+
+@app.get("/upload_sheet_music/")
 @app.post("/upload_sheet_music/")
-async def upload_sheet_music(file: UploadFile = File(...)):
-    if file.content_type not in ["application/pdf", "image/jpeg", "image/jpg", "image/png"]:
-        return {"error": "Please use a valid format"}
+async def upload_sheet_music(request: Request, file: UploadFile = File(None)):
     
+    MAX_FILE_SIZE = 10 * 1024 * 1024
+    
+    if request.method == "POST":
+        if not file:
+            raise HTTPException(status_code= 400, detail= "No file uploaded")
+        if file.content_type not in ["application/pdf", "image/jpeg", "image/jpg", "image/png"]:
+            raise HTTPException(status_code= 400, detail= "Upload a valid file")
+        
+        file_size = 0
+        file_data  = b""
+        async for chunk in file.stream():
+            file_size += len(chunk)
+            if file_size >= MAX_FILE_SIZE:
+                raise HTTPException(status_code= 400, detail= "File size exceeds 10mb limit")
+            file_data += chunk  
+        
+        return {"message": "File uploaded successfully"}
+    
+    return templates.TemplateResponse(
+        "upload_sheet_music.html", context={'request':request}
+    )
+    
+    
+    
+    
+"""
     # Process the file for OMR TODO
     result = await process_omr(file)
     return {"result": result}
+
 """
